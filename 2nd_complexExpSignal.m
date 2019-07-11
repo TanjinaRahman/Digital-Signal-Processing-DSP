efc2697ba471c153bcb164f225c81260d7fe4cdf
TanjinaRahman <31213441+TanjinaRahman@users.noreply.github.com>
2019-07-12 02:10:39 +0600

    Complex exponential signal

---
+++ 2nd_complexExpSignal.m
@@ -0,0 +1,20 @@
+% x(n) = r^n (cos*theta*n + j sin*theta*n)
+
+theta = pi/100;
+n=0:30;
+r=0.9;
+
+Xr = (r.^n).*cos(theta*n);
+Xi = (r.^n).*sin(theta*n);
+
+subplot(2,1,1);
+stem(n,Xr,'b','filled');
+xlabel('n');
+ylabel('x(n)');
+title('Real part');
+
+subplot(2,1,2);
+stem(n,Xi,'b','filled');
+xlabel('n');
+ylabel('x(n)');
+title('Imaginary part');
