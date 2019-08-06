f84f953cfc808dfc9088e0e77623df5a04dd4910
TanjinaRahman <31213441+TanjinaRahman@users.noreply.github.com>
2019-08-07 01:10:02 +0600

    Frequency Analysis Plotting

---
+++ 3.3frequencyANalysisPloting.m
@@ -0,0 +1,13 @@
+% Frequency analysis ploting (Homework 1)
+w = [0:1:500]*pi/500; % [0, pi] axis divided into 501 points.
+X = exp(j*w)+2+3.*exp(-j*w)+4.*exp(-j*2*w)+5.*exp(-j*3*w);
+magX = abs(X); angX = angle(X); realX = real(X); imagX = imag(X);
+subplot(2,2,1); plot(w/pi,magX); grid
+xlabel('frequency in pi units'); title('Magnitude Part'); ylabel('Magnitude')
+subplot(2,2,3); plot(w/pi,angX); grid
+xlabel('frequency in pi units'); title('Angle Part'); ylabel('Radians')
+subplot(2,2,2); plot(w/pi,realX); grid
+xlabel('frequency in pi units'); title('Real Part'); ylabel('Real')
+subplot(2,2,4); plot(w/pi,imagX); grid
+xlabel('frequency in pi units'); title('Imaginary Part'); ylabel('Imaginary')
+        
