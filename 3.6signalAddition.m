e795c6dacba35dae5551a3aae172e6d064aae97b
TanjinaRahman <31213441+TanjinaRahman@users.noreply.github.com>
2019-08-07 01:11:59 +0600

    DT signal addition

---
+++ 3.6signalAddition.m
@@ -0,0 +1,48 @@
+x = [1,2,3,1,4,5];
+nx = [-2,-1,0,1,2,3];
+y = [1,3,5,2];
+ny = [-1,0,1,2];
+
+lx=length(x);
+ly=length(y);
+if(nx(1)~=ny(1) || nx(lx)~=ny(ly))
+    f=0;
+    % n1 = min(nx(1),ny(1));
+    if(nx(1)<ny(1))
+        n1=nx(1);
+        extend1=ny(1)-nx(1);
+        f=1;
+    else
+        n1=ny(1);
+        extend1=nx(1)-ny(1);
+    end
+    % n2 = max(nx(lx),ny(ly));
+    g=0;
+    if(nx(lx)>ny(ly))
+        n2=nx(lx);
+        extend2=n2-ny(ly);
+        g=1;
+    else
+        n2=ny(ly);
+        extend2=n2-nx(lx);
+    end
+
+
+    n = n1:n2;
+
+    if(f==1)
+        y = [zeros(1,extend1),y];
+    else
+        x = [zeros(1,extend1),x];
+    end
+
+    if(g==1)
+        y = [y,zeros(1,extend2)];
+    else
+        x = [x,zeros(1,extend2)];
+    end
+
+end
+
+result = x+y;
+display(result);
