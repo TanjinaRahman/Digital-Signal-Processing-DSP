9b05933e5f64c3d4890c6d811dd5b325541d5bbd
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2019-09-13 19:19:41 +0600

    Sampling

--- 5sampling.m
+++ 5sampling.m
@@ -4,13 +4,15 @@ Ts = 1/Fs;
 
 tc = 0:1e-4:5/F;
 Xc = cos(2*pi*F*tc);
-figure(),plot(tc,Xc);
+subplot(2,1,1),plot(tc,Xc);
+title('input signal');
+
 
 td = 0:Ts:5/F;
 
 Xd = cos(2*pi*F*td); % f = F/Fs, 1/Fs = Ts so f = FTs ; 2pifn; td takes sample after Ts period
-figure(),stem(td,Xd);
-
+subplot(2,1,2),stem(td,Xd);
+title('sampled signal');
 N = length(td) % no. of sample
 
 
