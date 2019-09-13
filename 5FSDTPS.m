dc783a10ae2ddf80cb6b2d3846cca280b0d6222f
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2019-09-13 20:29:16 +0600

    Fourier Series for Discrete Time Periodic Signal

---
+++ 5FSDTPS.m
@@ -0,0 +1,33 @@
+xn = [1,1,0,0];
+N = length(xn);
+n=0:1:N-1
+subplot(2,2,1),stem(n,xn);
+title('Input');
+set(gca,'xticklabel',[0, 1, 2, 3])
+
+ck= zeros(1,N);
+for k=0:N-1
+    for n=0:N-1
+        ck(k+1) = ck(k+1) + (xn(n+1) * exp((-1i)*2*pi*k*n/N ));
+    end
+end
+ck = ck/N
+k=0:1:N-1
+subplot(2,2,2),stem(k,ck);
+title('Analysis');
+set(gca,'xticklabel',[0, 1, 2, 3])
+xlabel('k');
+ylabel('Ck');
+
+% Synthesis
+for n=0:N-1
+    for k=0:N-1
+        xn(n+1) = xn(n+1) + (ck(k+1) * exp(i*2*pi*k*n/N ));
+    end
+end
+n=0:1:N-1
+subplot(2,2,3),stem(n,xn);
+title('Synthesis');
+set(gca,'xticklabel',[0, 1, 2, 3])
+xlabel('n');
+ylabel('X(n)');
