58f8acf2d28cde3f564f00ba3cc5c5baf6b7c489
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2019-09-13 19:16:58 +0600

    Sine Train Pulse

---
+++ 5sintrain.m
@@ -0,0 +1,36 @@
+% Sampling and reconstruction demo
+clear,clc,close all;
+
+% Parameters
+F = 30;     % frequency of signal [Hz]
+Fs = 2*F;   % sampling rate [Hz]
+Ts = 1/Fs;  % sampling period [sec]
+
+% Generate "continuous time" signal and discrete time signal
+tc = 0:1e-4:5/F;        % CT axis
+xc = cos(2*pi*F*tc);    % CT signal
+td = 0:Ts:5/F;          % DT axis
+xd = cos(2*pi*F*td);    % DT signal
+N = length(td);         % number of samples
+
+% Reconstruction by using the formula:
+% xr(t) = sum over n=0,...,N-1: x(nT)*sin(pi*(t-nT)/T)/(pi*(t-nT)/T)
+% Note that sin(pi*(t-nT)/T)/(pi*(t-nT)/T) = sinc((t-nT)/T)
+% sinc(x) = sin(pi*x)/(pi*x) according to MATLAB
+xr = zeros(size(tc));
+sinc_train = zeros(N,length(tc));
+for t = 1:length(tc)
+    for n = 0:N-1
+        sinc_train(n+1,:) = sin(pi*(tc-n*Ts)/Ts)./(pi*(tc-n*Ts)/Ts);
+        xr(t) = xr(t) + xd(n+1)*sin(pi*(tc(t)-n*Ts)/Ts)/(pi*(tc(t)-n*Ts)/Ts);
+        
+    end
+end
+
+hold on
+grid on
+plot(tc,sinc_train);
+stem(td,xd)
+plot(tc,xc,'r', 'LineWidth',2)
+xlabel('Time [sec]')
+ylabel('Amplitude')
