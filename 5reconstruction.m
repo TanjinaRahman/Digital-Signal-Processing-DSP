f048b4a482fcaf4dbc47de91583e98b5d60a506f
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2019-09-13 19:24:50 +0600

    Signal Reconstruction

--- 5reconstruction.m
+++ 5reconstruction.m
@@ -1,4 +1,4 @@
-%% Sampling
+% Sampling
 F = 30; % frequency of CT
 Fs = 2*F;
 Ts = 1/Fs;
@@ -11,15 +11,13 @@ td = 0:Ts:5/F;
 Xd = cos(2*pi*F*td); % f = F/Fs, 1/Fs = Ts so f = FTs ; 2pifn; td takes sample after Ts period
 N = length(td) % no. of sample
 
-%% reconstruction
+% reconstruction
 x = zeros(length(tc));
 for t=1:length(tc)
     for n=0:N-1
         x(t) = x(t) + Xd(n+1)*sin(pi*(tc(t)-n*Ts)/Ts)/(pi*(tc(t)-n*Ts)/Ts);
     end
 end
-
-figure
 hold on;
 grid on;
 grid minor;
