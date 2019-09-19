c7e8d9830b2c42efaff5878045ca340ffdb87033
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2019-09-19 11:38:03 +0600

    Quize 5, ques 8

---
+++ 13058.m
@@ -0,0 +1,31 @@
+clear all;
+close all;
+clc;
+n=-3:3;
+xn = cos(exp(pi*n));
+N = length(xn);
+n= 0:1:N-1
+subplot(2,2,1),stem(n,xn);
+title('Input');
+%set(gca,'xticklabel',[0, 1, 2, 3])
+
+ck= zeros(1,N);
+for k=0:N-1
+    for n=0:N-1
+        ck(k+1) = ck(k+1) + (xn(n+1) * exp(-1i*2*pi*k*n/N ));
+    end
+end
+k=0:1:N-1
+subplot(2,2,3),stem(k,ck);
+title('DFT');
+%set(gca,'xticklabel',[0, 1, 2, 3])
+xlabel('k');
+ylabel('Ck');
+
+k=0:1:N-1
+ckk = abs(ck/N)
+subplot(2,2,4),stem(k,ckk);
+xlabel('k');
+ylabel('|Ck|');
+title('Spectra');
+%set(gca,'xticklabel',[0, 1, 2, 3])
