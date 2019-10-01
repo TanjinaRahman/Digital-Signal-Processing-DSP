c71e3b59f73a6814e77f1a4db36cabe04392044a
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2019-10-01 19:31:09 +0600

    labwork2 ques 2

---
+++ 13012.m
@@ -0,0 +1,15 @@
+%2.a
+B=[2,3,1,5;1,2,3,4;5,6,7,8]
+
+%2.b
+row2=B(2,:);
+col3=B(:,3);
+fprintf('\nElement of row2: \n');
+fprintf('%d\t',row2);
+fprintf('\nElement of col3: \n');
+fprintf('%d\n',col3);
+
+%2.c
+row3=B(3,:);
+fprintf('\nElement of row3: \n');
+fprintf('%d\t',row3);
